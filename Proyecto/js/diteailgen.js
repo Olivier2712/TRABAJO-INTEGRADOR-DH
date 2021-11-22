1925ac8febb2349556ae6297d632799595599ebc
mati-quintana-udesa <quintanam@udesa.edu.ar>
2021-11-22 14:29:29 -0300

    Generos y detalle de generos completos

--- Proyecto/js/diteailgen.js
+++ Proyecto/js/diteailgen.js
@@ -1,11 +1,62 @@
-let url = ("https://api.themoviedb.org/3/genre/movie/list?api_key=0f9656f2ce0988e0a6ad7ea1f7bb5506&language=en-US")
+//obtengo los valores que vienen por query string
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+const genero = urlParams.get('genero');
+const tipo = urlParams.get('tipo');
+let url;
 
-//fetch pelis
+if(tipo == 'series') {
+    url = 'https://api.themoviedb.org/3/discover/tv?api_key=0f9656f2ce0988e0a6ad7ea1f7bb5506&with_genres=' + id;
+}else{
+    url = 'https://api.themoviedb.org/3/discover/movie?api_key=0f9656f2ce0988e0a6ad7ea1f7bb5506&with_genres=' + id;
+}
+const titulo = document.querySelector('.genh1');
+
+titulo.innerText = genero;
+
+//consultar los datos en la api
+
 fetch(url)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    for (i=0; i < data.length; i++){
+    .then(function( resp ){
+        return resp.json();
+    })
+    .then(function( resp ){
         
-    }
-})
+        let listado = document.querySelector('.uldetallesgen');
+            
+        for(let i = 0; i < resp.results.length; i++) {
+
+            if(resp.results[i].backdrop_path != null) {
+
+                
+                //pregunto si es serie o pelicula entonces saco el nombre del objeto
+                if(tipo == 'series') {
+                    let id = resp.results[i].id;
+                    let nombre = resp.results[i].name;
+                    listado.innerHTML += `
+                    <li class="liclase">
+                        <a href="./series_detail.html?id=${ id }">     
+                            <img class="imag" src="https://image.tmdb.org/t/p/w342${resp.results[i].backdrop_path}" alt="">
+                        </a>
+                        <h3 class="h3detallesgen">${ nombre }</h3>
+                    </li>`;
+
+                }
+                
+                if(tipo == 'peliculas') {
+                    let id = resp.results[i].id;
+                    let nombre = resp.results[i].title;
+                    listado.innerHTML += `
+                    <li class="liclase">
+                        <a href="./movie_detail.html?id=${ id }">     
+                        <img class="imag" src="https://image.tmdb.org/t/p/w342${resp.results[i].backdrop_path}" alt="">
+                        </a>
+                        <h3 class="h3detallesgen">${ nombre }</h3>
+                    </li>`;
+
+                }
+                
+            }
+        }
+
+    });
--- Proyecto/js/genre.js
+++ Proyecto/js/genre.js
@@ -1,59 +1,39 @@
-let apiUrl = ("https://api.themoviedb.org/3/genre/movie/list?api_key=0f9656f2ce0988e0a6ad7ea1f7bb5506&language=en-US")
+//obtengo la url de generos con la api key
+let url = 'https://api.themoviedb.org/3/genre/movie/list?api_key=0f9656f2ce0988e0a6ad7ea1f7bb5506&language=en-US';
 
-//fetch pelis
-fetch(apiUrl)
+//consultar los datos en la api
 
-    .then(function (response) {
-        return response.json();
+fetch(url)
+    .then(function (resp) {
+        return resp.json();
     })
+    .then(function (resp) {
+        let listado = document.querySelector('#listaPeliculas');
 
-    .then(function (data) {
-        console.log(data.genres);
+        for (let i = 0; i < resp.genres.length; i++) {
+            let id = resp.genres[i].id;
+            let nombre = resp.genres[i].name;
+            listado.innerHTML += `<article><a class="clasea" href="../htmls/detail-genres.html?id=${id}&genero=${nombre}&tipo=peliculas">${nombre}</a></article>`;
+        }
+    });
 
-        for (let i = 0; i < 5; i++) {
 
-            let artpelis = document.querySelector('.peli').innerHTML += `
-                <ul class="genlis">
-                    <a href="../htmls/detail-genres.html"><li>${data.genres[i].name}</li></a>
-                </ul>
-             `;
+url = 'https://api.themoviedb.org/3/genre/tv/list?api_key=0f9656f2ce0988e0a6ad7ea1f7bb5506&language=en-US&page=1';
 
+fetch(url)
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (resp) {
+        let listado = document.querySelector('#listaSeries');
+
+        for (let i = 0; i < resp.genres.length; i++) {
+            let id = resp.genres[i].id;
+            let nombre = resp.genres[i].name;
+            listado.innerHTML += `<article><a class="clasea" href="./detail-genres.html?id=${id}&genero=${nombre}&tipo=series">${nombre}</a></article>`;
         }
 
-    })
-    .catch(function (error) {
-        console.log(`El error fue: ${error}`)
-    }
-    )
-
-
-//fetch series
-let apiUrll =('https://api.themoviedb.org/3/genre/tv/list?api_key=0f9656f2ce0988e0a6ad7ea1f7bb5506&language=en-US')
-
-fetch(apiUrll)
-
-    .then(function (response) {
-        return response.json();
-    })
-
-    .then(function (data) {
-        console.log(data.genres);
-
-        for (let i = 0; i < 5; i++) {
-
-            let artseri = document.querySelector('.seri').innerHTML += `
-                <ul class="genlis">
-                    <a href="../htmls/detail-genres.html"><li>${data.genres[i].name}</li></a>
-                </ul>
-             `;
-
-        }
-
-    })
-    .catch(function (error) {
-        console.log(`El error fue: ${error}`)
-    }
-    )
+    });
 
 
 
